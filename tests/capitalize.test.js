eea659d3beb13247b7560ad7e7dc94483d96f770
Andrii <youdronveduta@gmail.com>
2024-04-26 22:51:32 +0200

    update tests/capitalize.test.js

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,9 @@
+import { strict as assert } from 'node:assert';
 import { capitalize } from '../src/capitalize.js';
+import { access } from 'node:fs';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.strictEqual(capitalize(''), '');
+assert.strictEqual(capitalize('hello'), 'Hello');
 
 console.log('Все тесты пройдены!');
